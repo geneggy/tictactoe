373de0e2998ddc19f798ad4165ca8499840530d5
Eugene Gian <geneggy@gmail.com>
2021-08-22 22:26:09 -0700

    moved gameArr

--- script.js
+++ script.js
@@ -1,78 +1,3 @@
-const gameBoard = (() => {
-  let board = ["", "", "", "", "", "", "", "", ""];
-  const boardGridEl = document.querySelector(".game-grid");
-  const messageEl = document.querySelector(".game-message");
-  const resetBtnEl = document.querySelector(".game-reset-button");
-  board.forEach((item, index) => {
-    const boardSquareEl = document.createElement("div");
-    boardSquareEl.classList.add("board-square");
-    boardSquareEl.setAttribute("data-index", index);
-    boardGridEl.appendChild(boardSquareEl);
-  });
-
-  const boardSquares = Array.from(document.querySelectorAll(".board-square"));
-
-  boardSquares.forEach((square, index) => {
-    square.addEventListener("click", () => {
-      square.classList.add(game.currentPlayer.mark);
-      square.innerText = game.currentPlayer.mark;
-      board[index] = game.currentPlayer.mark;
-      game.turns -= 1;
-      console.log(game.turns);
-      console.log(game.checkWinner());
-      if (game.checkWinner()) {
-        messageEl.innerText = "Winner!";
-        const removePointerFromSqaures = boardSquares.filter((sq) => {
-          !(sq.style.pointerEvents = "none");
-        });
-        console.log(removePointerFromSqaures);
-        removePointerFromSqaures.forEach((square) => {
-          square.style.pointerEvents == "none";
-        });
-      } else if (game.turns == 0) {
-        messageEl.innerText = "tie!";
-      } else {
-        game.nextPlayer();
-      }
-      square.style.cursor = "not-allowed";
-      square.style.pointerEvents = "none";
-    });
-  });
-
-  function resetSquare(square, mark) {
-    square.classList.remove(mark);
-    square.innerText = "";
-    square.style.cursor = "crosshair";
-    square.style.pointerEvents = "auto";
-  }
-
-  function resetBoardGrid() {
-    board.forEach((item, index) => {
-      board[index] = "";
-    });
-    console.log(board);
-    messageEl.innerText = "";
-    boardSquares.forEach((square) => {
-      if (square.classList.contains("X")) {
-        resetSquare(square, "X");
-      }
-      if (square.classList.contains("O")) {
-        resetSquare(square, "O");
-      }
-      square.style.pointerEvents = "auto";
-    });
-
-    console.log(board);
-  }
-
-  resetBtnEl.addEventListener("click", () => {
-    resetBoardGrid();
-    game.resetGame();
-  });
-
-  return { board, boardSquares };
-})();
-
 //create players
 const playerFactory = (name, mark, turn) => {
   return { name, mark, turn };
@@ -80,6 +5,7 @@ const playerFactory = (name, mark, turn) => {
 
 //game status
 const game = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
   //initiate players
   const player1 = playerFactory("player1", "O", true);
   const player2 = playerFactory("player2", "X", false);
@@ -112,9 +38,9 @@ const game = (() => {
   function checkWinner() {
     return winCombos.some((combo) => {
       return (
-        gameBoard.board[combo[0]] === this.currentPlayer.mark &&
-        gameBoard.board[combo[1]] === this.currentPlayer.mark &&
-        gameBoard.board[combo[2]] === this.currentPlayer.mark
+        this.board[combo[0]] === this.currentPlayer.mark &&
+        this.board[combo[1]] === this.currentPlayer.mark &&
+        this.board[combo[2]] === this.currentPlayer.mark
       );
     });
   }
@@ -126,10 +52,80 @@ const game = (() => {
 
   //
   return {
+    board,
     currentPlayer,
     turns,
     nextPlayer,
     checkWinner,
     resetGame,
   };
+})();
+
+const gameBoard = (() => {
+  const boardGridEl = document.querySelector(".game-grid");
+  const messageEl = document.querySelector(".game-message");
+  const resetBtnEl = document.querySelector(".game-reset-button");
+
+  game.board.forEach((item, index) => {
+    const boardSquareEl = document.createElement("div");
+    boardSquareEl.classList.add("board-square");
+    boardSquareEl.setAttribute("data-index", index);
+    boardGridEl.appendChild(boardSquareEl);
+  });
+
+  const boardSquares = Array.from(document.querySelectorAll(".board-square"));
+
+  boardSquares.forEach((square, index) => {
+    square.addEventListener("click", () => {
+      square.classList.add(game.currentPlayer.mark);
+      square.innerText = game.currentPlayer.mark;
+      game.board[index] = game.currentPlayer.mark;
+      game.turns -= 1;
+      if (game.checkWinner()) {
+        messageEl.innerText = `${game.currentPlayer.mark} Wins!`;
+        const removePointerFromSqaures = boardSquares.filter((sq) => {
+          !(sq.style.pointerEvents = "none");
+        });
+        removePointerFromSqaures.forEach((square) => {
+          square.style.pointerEvents == "none";
+        });
+      } else if (game.turns == 0) {
+        messageEl.innerText = "tie!";
+      } else {
+        game.nextPlayer();
+      }
+      square.style.cursor = "not-allowed";
+      square.style.pointerEvents = "none";
+    });
+  });
+
+  function resetSquare(square, mark) {
+    square.classList.remove(mark);
+    square.innerText = "";
+    square.style.cursor = "crosshair";
+    square.style.pointerEvents = "auto";
+  }
+
+  function resetBoardGrid() {
+    game.board.forEach((item, index) => {
+      game.board[index] = "";
+    });
+    messageEl.innerText = "";
+    boardSquares.forEach((square) => {
+      if (square.classList.contains("X")) {
+        resetSquare(square, "X");
+      }
+      if (square.classList.contains("O")) {
+        resetSquare(square, "O");
+      }
+      square.style.pointerEvents = "auto";
+    });
+  }
+
+  resetBtnEl.addEventListener("click", () => {
+    resetBoardGrid();
+    game.resetGame();
+  });
+
+  return { boardSquares };
 })();
